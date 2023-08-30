d812bedaf25a9d4b80f72fc906557eb843f70e28
Ieva <ieva.maksvytyte@gmail.com>
2023-08-30 12:17:04 +0100

    Added ecr configs

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,28 +35,22 @@ resource "aws_security_group" "allow-traffic-to-dashboard"{
     name = "plants-vs-trainees-dashboard"
     vpc_id = "vpc-0e0f897ec7ddc230d"
     ingress {
-        from_port = 5439
-        to_port = 5439
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
         from_port   = 80
         to_port     = 80
         protocol    = "http"
         cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
+        from_port        = 5432
+        to_port          = 5432
+        protocol         = "tcp"
         cidr_blocks      = ["0.0.0.0/0"]
-  }
+    }
 }
 
 
 resource "aws_db_instance" "db-plants" {
-  identifier = "c8-plants-vs-trainees-db"
+  identifier = "plants-vs-trainees-db"
   allocated_storage    = 5
   db_name              = "plant_monitor"
   db_subnet_group_name = "public_subnet_group"
@@ -70,11 +64,98 @@ resource "aws_db_instance" "db-plants" {
 }
 
 
-# resource "aws_s3_bucket" "c8-plants-vs-trainees" {
-#   bucket = "c8-plants-vs-trainees"
-# }
+resource "aws_s3_bucket" "bucket" {
+  bucket = "plants-vs-trainees-long-term-storage"
+}
 
 
+resource "aws_ecs_cluster" "cluster" {
+  name = "plants-vs-trainees-cluster"
+}
+
+
+resource "aws_ecr_repository" "pipeline-repository" {
+    name = "plants-vs-trainees-pipeline-ecr"
+    image_tag_mutability = "MUTABLE"
+    image_scanning_configuration {
+        scan_on_push = true
+    }
+}
+
+
+resource "aws_ecr_repository" "dashboard-repository" {
+    name = "plants-vs-trainees-dashboard-ecr"
+    image_tag_mutability = "MUTABLE"
+    image_scanning_configuration {
+        scan_on_push = true
+    }
+}
+
+
+resource "aws_iam_role" "ecs_role" {
+  name = "ecs_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_ecs_task_definition" "dashboard-task-definition" {
+  family                = "plants-vs-trainees-dashboard-definition"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "0.5GB"
+  task_role_arn            = aws_iam_role.ecs_role.arn
+  execution_role_arn= "arn:aws:iam::129033205317:role/ecsTaskExecutionRole"
+  container_definitions = jsonencode(
+[
+  {
+    "name": "c8-ieva-tf-dashboard",
+    "image": "${aws_ecr_repository.dashboard-repository.url}:latest",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 8501,
+        "hostPort": 8501
+      }
+    ],
+    "environment" : [
+        {"name" : "DB_PASSWORD", "value" : var.db_password},
+        {"name" : "DB_USER", "value" : var.db_username},
+        {"name" : "DB_PORT", "value" : var.db_port},
+        {"name" : "DB_NAME", "value" : var.db_name},
+        {"name" : "ACCESS_KEY_ID", "value" : var.access_key_id},
+        {"name" : "SECRET_ACCESS_KEY", "value" : var.secret_access_key},
+        {"name" : "DB_HOST", "value" : var.db_host},
+        {"name" : "DB_SCHEMA", "value" : var.db_schema}
+      ]
+  }
+]
+)
+}
+
+
+resource "aws_ecs_task_definition" "name" {
+
+}
+
+resource "aws_ecs_service" "c8-pvst-pipeline-ecs" {
+
+}
 
 
 
