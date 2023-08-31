5e68be3501ddbf1d56f120712d7e3d3826ed985c
Ieva <ieva.maksvytyte@gmail.com>
2023-08-31 09:58:07 +0100

    fixed SG

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,6 +46,12 @@ resource "aws_security_group" "allow-traffic-to-dashboard"{
         protocol         = "tcp"
         cidr_blocks      = ["0.0.0.0/0"]
     }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+  }
 }
 
 
