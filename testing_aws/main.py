9a54d371d44d458b4f6b02fbe59fb6e790c3a298
Ieva <ieva.maksvytyte@gmail.com>
2023-08-31 12:30:57 +0100

    a working testing file for ecs service

--- testing_aws/main.py
+++ testing_aws/main.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 if __name__ == "__main__":
-    while True:
-        print("Hello World!")
+    st.title("🍦🏖️☀️Tasty Truck Treats:📊💰😈 Total Transactions & Tidbits")
+    st.markdown("_For stakeholders and interested parties; it's all about **trucks**_")
